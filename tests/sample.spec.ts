ea46bae3fa0b9d203e0bead0c4524f7662533659
Karthikeyan Shanmugam(UST <310970@ust.com>
2026-07-21 13:04:45 +0530

    test delete

--- tests/sample.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("Verify Playwright setup", async ({ page }) => {
-
-    await page.goto("https://example.com");
-
-    await expect(page).toHaveTitle(/Example Domain/);
-
-});
